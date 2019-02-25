3d02662f10dd5308ec3ae5b7e97c4d1c4e896e7a
John Cormican <jcormica@hpc09.tchpc.tcd.ie>
2019-02-25 15:18:22 +0000

    removed unnecessary parameters

--- caqr.h
+++ caqr.h
@@ -3,8 +3,10 @@
 
 #include "dense.h"
 
-void caqr(DenseMatrix* Rfin, DenseMatrix *Qfin, int nprocs, int myid, int b);
-void caQtA(DenseMatrix Q[], DenseMatrix *W, int myid, int comm_steps,int mylevel, int b, int j, int s);
+#include <mpi.h>
+
+void caqr(DenseMatrix* Rfin, int nprocs, int myid, int b, MPI_Comm comm, int nbrup, int nbrdown);
+void caQtA(DenseMatrix Q[], DenseMatrix *W, int myid, int comm_steps,int mylevel, int b, int j, int s, MPI_Comm comm);
 void Apply_QT(DenseMatrix* B,DenseMatrix* Q);
 
 #endif
